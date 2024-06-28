bd78241b53e699abd6c9627b81aa1052b1c0f425
huskyth <1622313177@qq.com>
2024-06-28 17:03:32 +0800

    feat: param add

--- ChromeDragon/Environment.py
+++ ChromeDragon/Environment.py
@@ -1,6 +1,21 @@
 class DragonEnvironment:
+    FRAME = 0.02
+    SPEED = 5
+    DRAGON_WIDTH = 30
+    DRAGON_HEIGHT = 50
+    JUMP_V = -300
+    FLOOR_Y = 800
+    G = 9.8 * 30  # g
+    SIZE = [1500, 1000]
+
     def __init__(self):
         self.is_dead = False
+        self.is_jump = False
+        self.jump_times = 2
+        self.dragon_x = 200
+        self.dragon_y = 760
+        self.raven_list = [[700, 1], [1700, 2]]
+        self.cactus_list = [[500, 2], [1000, 1], [1500, 1], [2000, 2]]
 
     def step(self, action):
         pass
