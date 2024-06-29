f23e49e4ecaf8641ef25ddc5bbb7432cb8f7d30f
huskyth <1622313177@qq.com>
2024-06-29 18:35:33 +0800

    feat: opt add

--- DQN/agent.py
+++ DQN/agent.py
@@ -2,6 +2,7 @@ import torch
 
 from DQN.buffer import DragonBuffer
 from DQN.model import DragonModel
+import torch.nn.functional as F
 
 
 class DragonAgent:
@@ -14,6 +15,9 @@ class DragonAgent:
 
         self.target_q_net.load_state_dict(self.q_net.state_dict())
         self.buffer = buffer
+        self.gamma = 0.99
+
+        self.optimizer = torch.optim.Adam(self.q_net.parameters(), lr=1e-2)
 
     @torch.no_grad()
     def get_action(self, state):
@@ -30,18 +34,27 @@ class DragonAgent:
 
         q_value = self.q_net(state).gather(1, action)
 
+        next_action = torch.argmax(self.q_net(next_state), dim=-1).unsqueeze(1)
+        q_target = self.target_q_net(next_state).gather(1, next_action) + reward
+
+        loss = F.mse_loss(q_target, q_value)
+
+        self.optimizer.zero_grad()
+        loss.backward()
+        self.optimizer.step()
+
 
 if __name__ == '__main__':
     s1 = torch.rand(1500, 1000, 3).cuda().unsqueeze(0)
     s2 = torch.rand(1500, 1000, 3).cuda().unsqueeze(0)
     s3 = torch.zeros(1500, 1000, 3).cuda().unsqueeze(0)
-    s4 = torch.rand(1500, 1000, 3).cuda().unsqueeze(0)
+    s4 = torch.zeros(1500, 1000, 3).cuda().unsqueeze(0)
 
-    buffer = DragonBuffer()
-    da = DragonAgent(buffer)
+    buffer_ = DragonBuffer()
+    da = DragonAgent(buffer_)
     action_test = da.get_action(s1)
     action_test_2 = da.get_action(s3)
-    buffer.push((s1, action_test, 1, s2))
-    buffer.push((s3, action_test_2, 0, s4))
+    buffer_.push((s1, action_test, 1, s2))
+    buffer_.push((s3, action_test_2, 0, s4))
 
     da.update()
