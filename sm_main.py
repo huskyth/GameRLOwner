2860f6bf60a5a923e357ea9fd36f9f5d37adf318
huskyth <1622313177@qq.com>
2024-07-08 23:08:42 +0800

    feat: train mode

--- sm_main.py
+++ sm_main.py
@@ -16,10 +16,7 @@ EPOCH = 100000000
 IS_TEST = False
 my_summary = MySummary(use_wandb=not IS_TEST)
 
-global_return = None
 IS_RENDER = False
-
-accumulate_return = 0
 
 
 @torch.no_grad()
@@ -35,18 +32,14 @@ def train(is_test):
     d_buffer = DragonBuffer()
     d_agent = DragonAgent(d_buffer, my_summary)
     # d_agent_.load()
-    global global_return
-    global accumulate_return
+    log_rate = 10
+    return_value = 0
     for epo in range(EPOCH):
-        if epo % 5 == 0 and not IS_TEST:
-            d_agent.save()
         is_terminate = False
         s = d_environment.reset()
         s = torch.tensor(s).permute((2, 0, 1))[None]
         step = 0
-        return_value = 0
-        action_list = ""
-        reward_list = ""
+
         c = Counter()
         while not is_terminate:
             step += 1
@@ -55,26 +48,21 @@ def train(is_test):
             s, reward, is_terminate, _ = d_environment.step(action)
             s = torch.tensor(s).permute((2, 0, 1))[None]
             d_buffer.push((pre_s, action, reward, s, is_terminate))
-
             return_value += reward
-            action_list += str(action) + ","
-            reward_list += str(reward) + ","
             c.update(str(action))
             if IS_RENDER:
                 env.render()
-        if global_return is None:
-            global_return = return_value
-        if not is_test:
-            global_return = 0.9 * global_return + 0.1 * return_value
-            my_summary.add_float(x=0, y=return_value, title="Return Value")
-            my_summary.add_float(x=0, y=step, title="Total Step")
-            my_summary.add_float(x=0, y=global_return, title="Smooth Reward")
+
+        if len(d_buffer.buffer) > 2000:
+            print("start training ...")
             d_agent.update()
             d_agent.save()
-            print(f"return value: {return_value}")
-            print(f"reward_list: {reward_list}")
-            print(action_list)
-            print(c)
+        if not is_test and epo + 1 % log_rate == 0:
+            my_summary.add_float(x=0, y=step, title="Total Step")
+            my_summary.add_float(x=0, y=return_value / log_rate, title="Smooth Reward")
+
+            return_value = 0
 
 
 if __name__ == '__main__':
+    train(IS_TEST)
